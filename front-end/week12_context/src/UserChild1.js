77d3ed03a7cb4edcb1a0733a3462c08da16bddbe
20cml <156444625+20cml@users.noreply.github.com>
2024-11-21 00:02:31 -0500

    week12

--- front-end/week12_context/src/UserChild1.js
+++ front-end/week12_context/src/UserChild1.js
@@ -3,13 +3,9 @@ import UserContext from "./UserContext";
 import { UserConsumer } from "./UserContext";
 import User1Child1 from "./User1Child1";
 
-
-
 export default function UserChild1() {
 
     let userCtx = useContext(UserContext)
-
-
     return (
         <div>
         <h3>User Child 1</h3>
